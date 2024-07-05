7a3ec89172fe8659d1410c34e3dd0ba6680ccda9
gferraro2019 <gferraro2016@gmail.com>
2024-07-05 16:07:52 +0200

    change nodes'name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-from ultrasonic_sensor import my_nodepub
+from ultrasonic_sensor import publisher
 
 package_name = "ultrasonic_sensor"
 
@@ -21,8 +21,8 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            "my_nodepub = ultrasonic_sensor.my_nodepub:main",
-            "my_nodesub = ultrasonic_sensor.my_nodesub:main",
+            "publisher = ultrasonic_sensor.publisher:main",
+            "subscriber = ultrasonic_sensor.subscriber:main",
         ],
     },
 )
